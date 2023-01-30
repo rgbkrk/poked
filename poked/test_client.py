4dc844c7918b9a383c3cd061aeac01d078343f84
Kyle Kelley <rgbkrk@gmail.com>
2023-01-29 17:40:32 -0800

    set up a basic test for the automatic caching

---
+++ poked/test_client.py
@@ -0,0 +1,57 @@
+# Time to test client.py
+# Path: poked/test_client.py
+
+import unittest
+import poked.client as client
+import poked.cache as cache
+
+from unittest.mock import patch, MagicMock
+
+
+class TestClient(unittest.IsolatedAsyncioTestCase):
+    def test_client(self):
+        # Just bootstrapping here
+        self.assertEqual(client.endpoint, "https://beta.pokeapi.co/graphql/v1beta")
+
+    @patch("poked.cache.get_cached_query", return_value=None)
+    @patch("poked.cache.cache_query", return_value=None)
+    async def test_autocache(self, mock_cache, mock_get):
+        # Set up our async query function
+        async def func(query):
+            return f"Queried: {query}"
+
+        # Wrap it with autocache
+        cached_query = client.autocache(func)
+
+        result = await cached_query("test")
+        self.assertEqual(result, "Queried: test")
+
+        self.assertTrue(mock_cache.called)
+        self.assertTrue(mock_get.called)
+
+        # Assert that it was called with the correct arguments
+        mock_cache.assert_called_with("test", "Queried: test")
+
+    @patch("poked.cache.get_cached_query", return_value=None)
+    @patch("poked.cache.cache_query", return_value=None)
+    async def test_autocache_cached(self, mock_cache, mock_get):
+        # Set up our async query function
+        async def func(query):
+            return f"Queried: {query}"
+
+        # Wrap it with autocache
+        cached_query = client.autocache(func)
+
+        # Mock the cache to return a value
+        mock_get.return_value = "Cached"
+
+        result = await cached_query("test")
+        self.assertEqual(result, "Cached")
+
+        # Assert that it was called with the correct arguments
+        mock_cache.assert_not_called()
+        mock_get.assert_called_with("test")
+
+
+if __name__ == "__main__":
+    unittest.main()
